2ffe03c671a148fb7b101abeb32267cf174cdb53
Paulo Roberto Campos de Vasconcelos <paulorcv@gmail.com>
2021-10-07 06:38:05 -0300

    Criando o painel para mostrar as fotos

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@
     <h1 class="centralizado">{{ titulo }}</h1>
     <ul>
       <li class="lista-fotos-item" v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo" />
+        <div class="painel">
+          <h2 class="painel-titulo">{{ foto.titulo }}</h2>
+          <div class="painel-conteudo">
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+          </div>
+        </div>
       </li>
     </ul>
   </div>
@@ -45,6 +50,33 @@ export default {
 }
 
 .lista-fotos-item {
-  display: inline;
+  display: inline-block;
+}
+
+/* estilo do painel */
+
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
+
+.imagem-responsiva {
+  width: 100%;
 }
 </style>
