3599421f887856d206e5de6f12584bbe936cc016
Paulo Roberto Campos de Vasconcelos <paulorcv@gmail.com>
2021-10-07 06:51:58 -0300

    Transformando o Painel em um componente reutilizável

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,22 @@
     <h1 class="centralizado">{{ titulo }}</h1>
     <ul>
       <li class="lista-fotos-item" v-for="foto of fotos">
-        <div class="painel">
-          <h2 class="painel-titulo">{{ foto.titulo }}</h2>
-          <div class="painel-conteudo">
-            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
-          </div>
-        </div>
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+        </meu-painel>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from "./components/shared/painel/Painel.vue";
+
 export default {
+  components: {
+    "meu-painel": Painel,
+  },
+
   data() {
     return {
       titulo: "Alurapic",
@@ -51,32 +54,5 @@ export default {
 
 .lista-fotos-item {
   display: inline-block;
-}
-
-/* estilo do painel */
-
-.painel {
-  padding: 0 auto;
-  border: solid 2px grey;
-  display: inline-block;
-  margin: 5px;
-  box-shadow: 5px 5px 10px grey;
-  width: 200px;
-  height: 100%;
-  vertical-align: top;
-  text-align: center;
-}
-
-.painel .painel-titulo {
-  text-align: center;
-  border: solid 2px;
-  background: lightblue;
-  margin: 0 0 15px 0;
-  padding: 10px;
-  text-transform: uppercase;
-}
-
-.imagem-responsiva {
-  width: 100%;
 }
 </style>
