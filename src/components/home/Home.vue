46060e01968a88ecb96e06b6ed2be180a15851d8
Paulo Roberto Campos de Vasconcelos <paulorcv@gmail.com>
2021-10-10 16:14:20 -0300

    Componente botão

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -11,6 +11,7 @@
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-painel :titulo="foto.titulo">
           <imagem-responsiva :url="foto.url" :titulo="foto.titulo" />
+          <meu-botao tipo="button" rotulo="REMOVER" />
         </meu-painel>
       </li>
     </ul>
@@ -20,11 +21,13 @@
 <script>
 import Painel from "../shared/painel/Painel.vue";
 import ImagemResponsiva from "../shared/imagem-responsiva/ImagemResponsiva.vue";
+import Botao from "../shared/botao/Botao.vue";
 
 export default {
   components: {
     "meu-painel": Painel,
     "imagem-responsiva": ImagemResponsiva,
+    "meu-botao": Botao,
   },
 
   data() {
