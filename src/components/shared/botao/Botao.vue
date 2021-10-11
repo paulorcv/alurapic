e44afee02dfccb9a4785d49bbcab4b513f8a691a
Paulo Roberto Campos de Vasconcelos <paulorcv@gmail.com>
2021-10-11 09:38:13 -0300

    Customizando o componente Botão

--- src/components/shared/botao/Botao.vue
+++ src/components/shared/botao/Botao.vue
@@ -1,10 +1,36 @@
 <template>
-  <button class="botao botao-perigo" :type="titulo">{{ rotulo }}</button>
+  <button
+    @click="disparaAcao()"
+    class="botao"
+    :class="estiloDoBotao"
+    :type="titulo"
+  >
+    {{ rotulo }}
+  </button>
 </template>
 
 <script>
 export default {
-  props: ["tipo", "rotulo"],
+  props: ["tipo", "rotulo", "confirmacao", "estilo"],
+  methods: {
+    disparaAcao() {
+      if (this.confirmacao) {
+        if (confirm("Confirma operação?")) {
+          this.$emit("botaoAtivado");
+        }
+        return;
+      } else {
+        this.$emit("botaoAtivado");
+      }
+    },
+  },
+
+  computed: {
+    estiloDoBotao() {
+      if (this.estilo == "padrao" || !this.estilo) return "botao-padrao";
+      if (this.estilo == "perigo") return "botao-perigo";
+    },
+  },
 };
 </script>
 
@@ -19,6 +45,11 @@ export default {
 
 .botao-perigo {
   background: firebrick;
+  color: white;
+}
+
+.botao-padrao {
+  background: blue;
   color: white;
 }
 
