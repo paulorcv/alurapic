9678eeb44058ca7016b61dfefd7d238bd9ab319b
Paulo Roberto Campos de Vasconcelos <paulorcv@gmail.com>
2021-10-11 16:55:15 -0300

    Classe de Foto

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -41,6 +41,7 @@
 <script>
 import ImagemResponsiva from "../shared/imagem-responsiva/ImagemResponsiva.vue";
 import Botao from "../shared/botao/Botao.vue";
+import Foto from "../../domain/Foto";
 
 export default {
   components: {
@@ -50,22 +51,14 @@ export default {
 
   data() {
     return {
-      foto: {
-        titulo: "",
-        url: "",
-        descricao: "",
-      },
+      foto: new Foto(),
     };
   },
 
   methods: {
     grava() {
       console.log("Enviar os dados para a api");
-      this.foto = {
-        titulo: "",
-        url: "",
-        descricao: "",
-      };
+      this.foto = new Foto();
     },
   },
 };
