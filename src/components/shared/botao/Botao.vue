dddf1a6088cb3f6b7fbd065a52124bd5fa6cb3e8
Paulo Roberto Campos de Vasconcelos <paulorcv@gmail.com>
2021-10-11 09:57:15 -0300

    Definindo as propriedades do componente

--- src/components/shared/botao/Botao.vue
+++ src/components/shared/botao/Botao.vue
@@ -11,7 +11,18 @@
 
 <script>
 export default {
-  props: ["tipo", "rotulo", "confirmacao", "estilo"],
+  props: {
+    tipo: {
+      required: true,
+      type: String,
+    },
+    rotulo: {
+      required: true,
+      type: String,
+    },
+    confirmacao: Boolean,
+    estilo: String,
+  },
   methods: {
     disparaAcao() {
       if (this.confirmacao) {
