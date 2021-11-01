017c1db6d28125e518fb23d3adee8820e5d18145
Paulo Roberto Campos de Vasconcelos <paulorcv@gmail.com>
2021-11-01 17:05:32 -0300

    Validação de formulários utilizando o VeeValidate

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -8,12 +8,16 @@
     <form @submit.prevent="grava()">
       <div class="controle">
         <label for="titulo">TÍTULO</label>
-        <input id="titulo" autocomplete="off" v-model.lazy="foto.titulo" />
+        <input name="titulo" v-model="foto.titulo" id="titulo" autocomplete="off" 
+        v-validate data-vv-rules="required|min:3|max:30" data-vv-as="título" >
+        <span class="erro" v-show="errors.has('titulo')">{{ errors.first('titulo') }}</span>
       </div>
 
       <div class="controle">
         <label for="url">URL</label>
-        <input id="url" autocomplete="off" v-model.lazy="foto.url" />
+        <input name="url" id="url" autocomplete="off" v-model="foto.url" 
+        v-validate data-vv-rules="required" >
+        <span class="erro" v-show="errors.has('url')">{{ errors.first('url') }}</span>
         <imagem-responsiva
           v-show="foto.url"
           :url="foto.url"
@@ -62,16 +66,22 @@ export default {
 
   methods: {
     grava() {
-      console.log(this.foto);
 
-      // o método save realiza um POST por debaixo dos panos enviado os dados passado como parâmetro
+      this.$validator
+      .validateAll()
+      .then(success => {
+        if(success){
       this.service.cadastra(this.foto).then(
         () => {
           if(this.id) this.$router.push({ name: 'home'})
           this.foto = new Foto()
         },
         (err) => console.log(err)
-      );
+      );          
+        }
+      })
+
+
     },
   },
   created() {
@@ -106,5 +116,9 @@ export default {
 
 .centralizado {
   text-align: center;
+}
+
+.erro {
+  color: red
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import VueResource from "vue-resource";
 import VueRouter from "vue-router";
 import App from "./App.vue";
 import { routes } from "./routes";
-// import "./directives/Transform";
+import VeeValidate from "vee-validate";
+import "./directives/Transform";
 
 Vue.use(VueResource);
 Vue.use(VueRouter);
+
 
 const router = new VueRouter({
   routes,
@@ -15,6 +17,7 @@ const router = new VueRouter({
 
 Vue.http.options.root = 'http://localhost:3000';
 
+Vue.use(VeeValidate);
 
 new Vue({
   el: "#app",
